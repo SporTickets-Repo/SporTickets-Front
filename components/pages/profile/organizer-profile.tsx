136bcdff5638cec18cf54462d8195af5a9f89617
Alexandre Beck <alexandrebeck2003@gmail.com>
2025-03-17 15:19:02 -0300

    feat: implement tabs on profile screen

--- components/pages/profile/organizer-profile.tsx
+++ components/pages/profile/organizer-profile.tsx
@@ -1,3 +1,26 @@
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export function OrganizerProfile() {
-  return <h1>Organizer Profile</h1>;
+  return (
+    <div className="mt-10">
+      <Tabs defaultValue="events" className="w-full">
+        <TabsList className="w-full justify-start">
+          <TabsTrigger value="events">Eventos</TabsTrigger>
+          <TabsTrigger value="metrics">Métricas</TabsTrigger>
+          <TabsTrigger value="tickets">Ingressos</TabsTrigger>
+        </TabsList>
+        <Separator className="my-4" />
+        <TabsContent value="events">
+          <p>Eventos</p>
+        </TabsContent>
+        <TabsContent value="metrics">
+          <p>Métricas</p>
+        </TabsContent>
+        <TabsContent value="tickets">
+          <p>Ingressos</p>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
--- components/pages/profile/user-profile.tsx
+++ components/pages/profile/user-profile.tsx
@@ -1,3 +1,18 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Separator } from "@radix-ui/react-separator";
+
 export function UserProfile() {
-  return <h1>User Profile</h1>;
+  return (
+    <div className="mt-10">
+      <Tabs defaultValue="tickets" className="w-full">
+        <TabsList className="w-full justify-start">
+          <TabsTrigger value="tickets">Ingressos</TabsTrigger>
+        </TabsList>
+        <Separator className="my-4" />
+        <TabsContent value="tickets">
+          <p>Ingressos</p>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
