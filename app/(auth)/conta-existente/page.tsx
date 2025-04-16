f8ba468dcfb915d65514128dd34a2e8c7f30d829
Alexandre Beck <alexandrebeck2003@gmail.com>
2025-04-16 13:02:51 -0300

    feat: wrap ExistingAccountForm in Suspense for improved loading handling

--- app/(auth)/conta-existente/page.tsx
+++ app/(auth)/conta-existente/page.tsx
@@ -1,9 +1,12 @@
 import { ExistingAccountForm } from "@/components/existing-account-form";
+import { Suspense } from "react";
 
 export default function ExistingAccountPage() {
   return (
     <div className="flex flex-1 justify-center items-center bg-muted">
-      <ExistingAccountForm />
+      <Suspense>
+        <ExistingAccountForm />
+      </Suspense>
     </div>
   );
 }
