8dce288c28d59d1c2fc60210c58deb41128cb011
Alexandre Beck <alexandrebeck2003@gmail.com>
2025-04-22 11:04:05 -0300

    fix: update Image component height for better responsiveness

--- components/pages/event/event-header.tsx
+++ components/pages/event/event-header.tsx
@@ -13,7 +13,7 @@ export default function EventHeader({ alt, image }: EventHeaderProps) {
         alt={alt}
         width={800}
         height={200}
-        className="h-[30vh] w-full object-cover"
+        className="lg:h-[50vh] w-full object-cover"
         unoptimized
         priority
       />
