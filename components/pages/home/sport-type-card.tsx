c8e9355dbdb292a2a96ce074ff8d61903b9d2f92
LucasHARosa <l.ucas.henrique.a.r@gmail.com>
2025-04-25 00:05:15 -0300

    fix: ajustar tamanhos e espaçamentos em componentes de busca e cartão de tipo de esporte

--- components/pages/home/sport-type-card.tsx
+++ components/pages/home/sport-type-card.tsx
@@ -16,7 +16,7 @@ export default function SportTypeCard({
     <Card
       className={`bg-white/10 text-white border-0 backdrop-blur ${className} cursor-pointer hover:bg-white/20 `}
     >
-      <CardContent className="flex items-center justify-center py-6 sm:py-8 px-0">
+      <CardContent className="flex items-center justify-center py-4 md:py-8 px-0">
         <Icon size={24} />
         <h3 className="font-bold ml-2 text-sm sm:text-base">{title}</h3>
       </CardContent>
